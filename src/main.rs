b69e9adef442b4c39c65360dbf3cca1fe3d89bd8
Alston Dsouza <alstondsouza35@gmail.com>
2026-05-30 14:22:53 -0700

    using timer for raindrop spawning

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 
-// Components
 #[derive(Component)]
 struct Player;
 
@@ -10,14 +9,17 @@ struct Bug;
 #[derive(Component)]
 struct Raindrop;
 
-// Resource to track score
 #[derive(Resource)]
 struct Score(u32);
+
+#[derive(Resource)]
+struct RainTimer(Timer);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(Score(0))
+        .insert_resource(RainTimer(Timer::from_seconds(0.2, TimerMode::Repeating)))
         .add_systems(Startup, setup)
         .add_systems(
             Update,
@@ -32,12 +34,9 @@ fn main() {
         .run();
 }
 
-// Setup scene
 fn setup(mut commands: Commands) {
-    // Camera
     commands.spawn(Camera2dBundle::default());
 
-    // Player
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
@@ -50,7 +49,6 @@ fn setup(mut commands: Commands) {
         Player,
     ));
 
-    // Bug
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
@@ -65,14 +63,12 @@ fn setup(mut commands: Commands) {
     ));
 }
 
-// PLAYER MOVEMENT
 fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<&mut Transform, With<Player>>,
     time: Res<Time>,
 ) {
     let mut transform = query.single_mut();
-
     let mut direction = Vec3::ZERO;
 
     if keyboard_input.pressed(KeyCode::KeyW) {
@@ -92,7 +88,6 @@ fn player_movement(
     transform.translation += direction.normalize_or_zero() * speed * time.delta_seconds();
 }
 
-// BUG COLLECTION
 fn collect_bug(
     mut commands: Commands,
     mut score: ResMut<Score>,
@@ -102,9 +97,7 @@ fn collect_bug(
     let player_transform = player_query.single();
 
     for (entity, bug_transform) in bug_query.iter() {
-        let distance = player_transform
-            .translation
-            .distance(bug_transform.translation);
+        let distance = player_transform.translation.distance(bug_transform.translation);
 
         if distance < 40.0 {
             commands.entity(entity).despawn();
@@ -114,11 +107,13 @@ fn collect_bug(
     }
 }
 
-// SPAWN RAIN
-fn spawn_raindrops(mut commands: Commands, time: Res<Time>) {
-    // simple timer using elapsed time
-    if (time.elapsed_seconds() * 5.0) as i32 % 10 == 0 {
-        let x = (time.elapsed_seconds().sin() * 300.0) as f32;
+fn spawn_raindrops(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut timer: ResMut<RainTimer>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        let x = time.elapsed_seconds().sin() * 300.0;
 
         commands.spawn((
             SpriteBundle {
@@ -135,7 +130,6 @@ fn spawn_raindrops(mut commands: Commands, time: Res<Time>) {
     }
 }
 
-// MOVE RAIN DOWN
 fn move_raindrops(
     mut commands: Commands,
     mut query: Query<(Entity, &mut Transform), With<Raindrop>>,
@@ -144,14 +138,12 @@ fn move_raindrops(
     for (entity, mut transform) in query.iter_mut() {
         transform.translation.y -= 400.0 * time.delta_seconds();
 
-        // Remove if off screen
         if transform.translation.y < -350.0 {
             commands.entity(entity).despawn();
         }
     }
 }
 
-// COLLISION WITH PLAYER
 fn check_rain_collision(
     mut player_query: Query<&mut Transform, (With<Player>, Without<Raindrop>)>,
     rain_query: Query<&Transform, (With<Raindrop>, Without<Player>)>,
@@ -159,16 +151,11 @@ fn check_rain_collision(
     let mut player_transform = player_query.single_mut();
 
     for rain_transform in rain_query.iter() {
-        let distance = player_transform
-            .translation
-            .distance(rain_transform.translation);
+        let distance = player_transform.translation.distance(rain_transform.translation);
 
         if distance < 30.0 {
             println!("Game over!");
-
-            // Reset player to center
             player_transform.translation = Vec3::ZERO;
-
             break;
         }
     }
